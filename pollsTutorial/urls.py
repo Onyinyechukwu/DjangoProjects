d82ea7f2c8a8ce0115ac87fb225b4f2f2693e32f
Sankore-Onyinye <onyinye@sankore.com>
2018-11-08 11:58:35 +0100

    stage 2 changes just before automated testing

--- pollsTutorial/urls.py
+++ pollsTutorial/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'pollsTutorial'
+
 urlpatterns = [
-    path('',views.index, name = 'index'),
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('',views.IndexView.as_view(), name = 'index'),
+    path('<int:pk>/', views.DetailsView.as_view(), name='detail'),
     
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
    
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- pollsTutorial/views.py
+++ pollsTutorial/views.py
@@ -1,24 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
-from django.http import  HttpResponse
-from .models import Question
+from django.http import Http404
+from django.http import  HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('pollsTutorial/index.html')
-    context = {'latest_question_list':latest_question_list}
-    return HttpResponse((template.render(context,request)))   
-    #return HttpResponse("Welcome To This Project")
+from .models import Question, Choice
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+class IndexView(generic.ListView):
+    template_name = 'pollsTutorial/index.html'
+    context_object_name = 'latest_question_list'
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailsView(generic.DetailView):
+    model = Question
+    template_name = 'pollsTutorial/details.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'pollsTutorial/results.html'
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'pollsTutorial/details.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('pollsTutorial:results', args=(question.id,)))
 
 
 
